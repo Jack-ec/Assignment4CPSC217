0f41243617b3d2779da80c4b5b6823377793c209
jchid <jchidlaw@gmail.com>
2024-12-02 21:34:52 -0700

    get_file_names

--- game_functions.py
+++ game_functions.py
@@ -1,5 +1,13 @@
 def get_file_names():
     with open("file_names.txt", "r") as file:
         content = file.read()
+        print(str(content))
     return content
+
+
+def read_config(config_file_name):
+    with open(config_file_name + ".txt", "r") as file:
+        content = file.read()
+
+        order_map = {value: index for index, value in enumerate(READ_CONFIG_VALUE_ORDER)}
 
